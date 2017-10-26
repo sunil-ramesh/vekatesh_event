14351d8a2efc6ab86c07ae1e8083606a788962bb
sunil-ramesh <skramesh@qwinix.io>
2017-10-26 13:13:45 +0530

    changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,9 +20,9 @@ set :stage, :production
 set :branch, :master
 set :deploy_to,  '/home/deploy/u01/apps/qwinix/pictour'
 set :user, "deploy"
-role :app, %w{deploy@52.90.14.191}}
-role :web, %w{deploy@52.90.14.191}}
-role :db,  %w{deploy@52.90.14.191}}
+role :app, %w{deploy@52.90.14.191}
+role :web, %w{deploy@52.90.14.191}
+role :db,  %w{deploy@52.90.14.191}
 server '52.90.14.191}', roles: %w{:web, :app, :db}, user: 'deploy'
 set :ssh_options,
  {keys: %w(~/.ssh/id_rsa),
