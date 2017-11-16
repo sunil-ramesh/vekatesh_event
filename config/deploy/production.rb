f0af3e4ea893158f4a1fe0dacfa2ad18b7da25d2
sunil-ramesh <skramesh@qwinix.io>
2017-11-16 10:43:46 +0530

    ip address changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,22 +16,31 @@
 # group is considered to be the first unless any hosts have the primary
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
-set :stage, :production
-set :branch, :master
-set :deploy_to,  '/home/deploy/u01/apps/qwinix/pictour'
-set :user, "deploy"
-role :app, %w{deploy@52.90.14.191}
-role :web, %w{deploy@52.90.14.191}
-role :db,  %w{deploy@52.90.14.191}
-server '52.90.14.191}', roles: %w{:web, :app, :db}, user: 'deploy'
-set :ssh_options,
- {keys: %w(~/.ssh/id_rsa),
-	auth_methods: %w(publickey)}
+
 
 # role :app, %w{sunilexample.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
+set :stage, :production
+set :branch, :master
+set :deploy_to, '/home/deploy/u01/apps/qwinix/event'
 
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+role :app, %w{deploy@52.55.93.252}
+role :web, %w{deploy@52.55.93.252}
+role :db, %w{deploy@52.55.93.252}
+server '52.55.93.252', roles: %w{:web, :app, :db}, user: 'deploy'
+
+set :ssh_options, {
+   #verbose: :debug,
+   keys: %w(~/.ssh/id_rsa),
+   auth_methods: %w(publickey)
+}
 
 
 # Configuration
