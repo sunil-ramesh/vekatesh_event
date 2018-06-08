d0cef19c1b4a1aeb02b5565a11cda5583beaa3b5
nikhil405 <npraksh@qwinix.io>
2018-06-08 17:22:15 +0530

    commited

--- config/deploy/it.rb
+++ config/deploy/it.rb
@@ -1,7 +1,7 @@
 set :stage, :it
 set :branch, :nikhil
 # set :rails_env, :it
-set :deploy_to, '/u02/apps/qwinix/vekatesh_event'
+set :deploy_to, '/u01/apps/qwinix/bug_tracker'
 # set :log_level, :debug
 
 # Extended Server Syntax
@@ -10,10 +10,10 @@ set :deploy_to, '/u02/apps/qwinix/vekatesh_event'
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-role :app, %w{qwinix@192.168.2.106}
-role :web, %w{qwinix@192.168.2.106}
-role :db, %w{qwinix@192.168.2.106}
-server '192.168.2.106', roles: %w{:web, :app, :db}, user: 'qwinix'
+role :app, %w{deploy@18.218.168.189}
+role :web, %w{deploy@18.218.168.189}
+role :db, %w{deploy@18.218.168.189}
+server '18.218.168.189', roles: %w{:web, :app, :db}, user: 'deploy'
 
 set :ssh_options, {
    #verbose: :debug,
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,22 +1,22 @@
-set :stage, :production
-set :branch, :nikhil
-# set :rails_env, :it
-set :deploy_to, '/u02/apps/qwinix/vekatesh_event'
-# set :log_level, :debug
+# set :stage, :production
+# set :branch, :nikhil
+# # set :rails_env, :it
+# set :deploy_to, '/u01/apps/qwinix/event_schedule'
+# # set :log_level, :debug
 
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server
-# definition into the server list. The second argument
-# something that quacks like a hash can be used to set
-# extended properties on the server.
-role :app, %w{qwinix@192.168.2.106}
-role :web, %w{qwinix@192.168.2.106}
-role :db, %w{qwinix@192.168.2.106}
-server '192.168.2.106', roles: %w{:web, :app, :db}, user: 'qwinix'
+# # Extended Server Syntax
+# # ======================
+# # This can be used to drop a more detailed server
+# # definition into the server list. The second argument
+# # something that quacks like a hash can be used to set
+# # extended properties on the server.
+# ole :app, %w{deploy@18.218.168.189}
+# role :web, %w{deploy@18.218.168.189}
+# role :db, %w{deploy@18.218.168.189}
+# server '18.218.168.189', roles: %w{:web, :app, :db}, user: 'deploy'
 
-set :ssh_options, {
-   #verbose: :debug,
-   keys: %w(~/.ssh/id_rsa),
-   auth_methods: %w(publickey)
-}
+# set :ssh_options, {
+#    #verbose: :debug,
+#    keys: %w(~/.ssh/id_rsa),
+#    auth_methods: %w(publickey)
+# }
