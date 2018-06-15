21f37ffa55b1aae47b6d0f17733a513a91cf2f99
nikhil405 <npraksh@qwinix.io>
2018-06-15 16:12:10 +0530

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -23,7 +23,7 @@
 # role :db,  %w{deploy@example.com}
 set :stage, :production
 set :branch, :master
-set :deploy_to, '/u01/apps/qwinix/sunil_event'
+set :deploy_to, '/u01/apps/qwinix/events_deploy'
 
 # Extended Server Syntax
 # ======================
@@ -31,10 +31,10 @@ set :deploy_to, '/u01/apps/qwinix/sunil_event'
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-role :app, %w{deploy@18.191.179.1}
-role :web, %w{deploy@18.191.179.1}
-role :db, %w{deploy@18.191.179.1}
-server '18.191.179.1', roles: %w{:web, :app, :db}, user: 'deploy'
+role :app, %w{deploy@18.191.228.32}
+role :web, %w{deploy@18.191.228.32}
+role :db, %w{deploy@18.191.228.32}
+server '18.191.228.32', roles: %w{:web, :app, :db}, user: 'deploy'
 
 set :ssh_options, {
    #verbose: :debug,
